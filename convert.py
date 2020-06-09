80152682d7ee2361b558f7f7264bfd24f3a4970e
leo <leobloess@gmail.com>
2020-06-09 16:23:26 +0200

    Start using ascii lut

--- convert.py
+++ convert.py
@@ -1,6 +1,10 @@
 from PIL import Image
-image = Image.open("syth.jpg")
-basewidth = 1920
+import sys
+import time
+def printf(format, *args):
+    sys.stdout.write(format % args)
+image = Image.open("blahaj.jpeg")
+basewidth = 100
 wpercent = (basewidth/float(image.size[0]))
 hsize = int((float(image.size[1])*float(wpercent)))
 image = image.resize((basewidth,hsize), Image.ANTIALIAS)
@@ -8,14 +12,16 @@ image = image.convert('1', dither=1)
 image.save('dit.gif')
 width, height = image.size
 for y in range(height):
-    print(end="{")
     for x in range(width):
         l = image.getpixel((x,y))
         if l == 0:
-            print(end="1")
+            print("1")
+#            time.sleep(0.01)
         else:
-            print(end="0")
-        if x != width-1:
-            print(end=",")
+            print("0")
+#            time.sleep(0.01)
+        #if x != width-1:
+        #    print(end=",")
         #print(f"x = {x}, y = {y}, {l}")
-    print("} ,")
+    print("2")
+print("3")
--- src/main-ascii.cpp
+++ src/main-ascii.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include "BasicStepperDriver.h"
-
+#include "ascii-map.h"
 #define MOTOR_STEPS 200		// Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
 #define RPM 100			//Seames to be the best
 #define MICROSTEPS 1		// 1=full step, 2=half step etc.
@@ -27,13 +27,17 @@ int currentHeadPos = 0;		//Save the current head position (x coord)
 
 //The char table with all avalable chars and ther position on the dial/wheel
 String charTable = "_W)UIN?D\"C*R$LTAY&VZSPFB~E0987654321+~>^f@~wjmlv-,.zobudiaesrngchpky~~~~qxt#~<~`~~G~%JQ~X!O/K(H\'M:;= \n\r";
-
+byte escapeCounter=0;
 long previousMillis=0;
 
 void printChar(char charToPrint);
+void printString(String stringToPrint, bool ln);
+void printPixelRelativ (int x, int y);
+void hammerHit ();
+void newLine();
+void xyMove(int x, int y);
 
-void dialReset ()
-{
+void dialReset (){
 tape.enable ();
 head.enable ();
 
@@ -75,15 +79,58 @@ lastChar=0;
 
 }
 
-void
-recvWithEndMarker (){
+void recvSerialData (){
 	unsigned long currentMillis = millis();
 	int rc = 0;
 	if (Serial.available () > 0){
 		previousMillis = currentMillis;
 		rc = Serial.read ();
-		if (rc >= 0){
+
+		if (rc >= 0) {
 			printChar((char)rc);
+		}
+		if ((char)rc=='<') {
+			escapeCounter++;
+			if (escapeCounter==5) {
+				printString("Dot mode.",true);
+				int blankSpace = 0;
+				//(char)rc=='^'||(char)rc=='1'||(char)rc=='0'
+				rc=0;
+				head.enable();
+				head.move(100);
+				currentHeadPos=0;
+				while ((char)rc!='3') {
+					rc=0;
+					rc = Serial.read ();
+					if ((char)rc=='1'){
+						head.move (blankSpace);
+						currentHeadPos += blankSpace;
+						blankSpace = 0;
+						delay (1);
+						printPixelRelativ (0, 0);
+						blankSpace += 6;
+					}
+					if ((char)rc=='0'){
+						blankSpace += 6;
+					}
+					if ((char)rc=='2'){
+						blankSpace = 0;
+						head.enable ();
+						head.move (-currentHeadPos);
+						currentHeadPos = 0;
+						delay (1);
+						feed.enable ();
+						feed.move (5);
+					}
+
+				}
+				newLine();
+				head.move(-100);
+				currentHeadPos=0;
+				escapeCounter=0;
+			}
+		}else{
+			escapeCounter=0;
 		}
 	}else{
 		if (currentMillis - previousMillis >= 50000) {
@@ -126,8 +173,8 @@ hammerHit ()
 newLine ()
 {
 	head.enable ();
-	Serial.println ("backing of ");
-	Serial.print (-currentHeadPos);
+	//Serial.println ("backing of ");
+	//Serial.print (-currentHeadPos);
 	head.move (-currentHeadPos);
 	currentHeadPos = 0;
 	//head.disable();
@@ -141,24 +188,24 @@ newLine ()
    Method for printing a singe char at the current position and than advance the head to the next position or doing some special char stuff such as space or CR
  */
 void printChar (char charToPrint) {
-	int charNumber = charTable.lastIndexOf (charToPrint);
-	if (charNumber >= 100 && charNumber >= 0)
+	char charNumber = ascii_lookup[charToPrint];
+	if (charNumber >= 100)
 	{				//Some special chars:
-		Serial.print ("[INFO]Special char :");
-		Serial.print (charToPrint);
-		Serial.print (" char num: ");
-		Serial.println (charNumber);
+		//Serial.print ("[INFO]Special char :");
+		//Serial.print (charToPrint);
+		//Serial.print (" char num: ");
+		//Serial.println (charNumber);
 		switch (charNumber)
 		{
 			case 100:		//White space
-				Serial.println ("space");
+				//Serial.println ("space");
 				head.enable ();
 				head.move (charSpaceing);
 				currentHeadPos += charSpaceing;
 				//head.disable();
 				break;
-			case 101:		//Cardrige return(reset head to 0<x=0> and advance the paper<y-1>)
-				Serial.println ("CR");
+			case 101:		//Cardrige return(reset head to 0<x=0> and advance the paper<y-1>) LF
+				//Serial.println ("CR");
 				head.enable ();
 				head.move (-currentHeadPos);
 				currentHeadPos = 0;
@@ -167,28 +214,89 @@ void printChar (char charToPrint) {
 				feed.move (40);
 				feed.disable ();
 				break;
+			case 102://[
+				head.enable();
+				feed.enable();
+				xyMove(0,6);
+				printPixelRelativ(0,0);
+				printPixelRelativ(0,-6);
+				printPixelRelativ(0,-6);
+				printPixelRelativ(0,-6);
+				printPixelRelativ(0,-6);
+				printPixelRelativ(4,0);
+				printPixelRelativ(0,24);
+				xyMove(-4+charSpaceing,-6);
+				head.disable();
+				feed.disable();
+				break;
+			case 103://]
+				head.enable();
+				feed.enable();
+				xyMove(0,6);
+				printPixelRelativ(0,0);
+				printPixelRelativ(4,0);
+				printPixelRelativ(0,-6);
+				printPixelRelativ(0,-6);
+				printPixelRelativ(0,-6);
+				printPixelRelativ(0,-6);
+				printPixelRelativ(-4,0);
+				xyMove(charSpaceing,24-6);
+				head.disable();
+				feed.disable();
+				break;
+			case 104://'\'
+				head.enable();
+				feed.enable();
+				xyMove(-6,-16);
+				printPixelRelativ(0,0);
+				printPixelRelativ(3,4);
+				printPixelRelativ(3,4);
+				printPixelRelativ(3,4);
+				printPixelRelativ(3,4);
+				xyMove(-6+charSpaceing,0);
+				head.disable();
+				feed.disable();
+				break;
+			case 105://'{'
+				printChar('(');
+				head.enable();
+				feed.enable();
+				xyMove(-charSpaceing-5,-4);
+				printPixelRelativ(0,0);
+				xyMove(5+charSpaceing,4);
+				head.disable();
+				feed.disable();
+				break;
+			case 255:
+				printChar('?');
+				//printString(String(charToPrint),false);
+				//Serial.print(charToPrint);
+				break;
+
+
 		}
+
 	} else if (charNumber >= 0) {
-		Serial.print ("[INFO]Printing a char :");
-		Serial.print (charToPrint);
-		Serial.print (" Pos on wheel: ");
-		Serial.println (charNumber);
+		//Serial.print ("[INFO]Printing a char :");
+		//Serial.print (charToPrint);
+		//Serial.print (" Pos on wheel: ");
+		//Serial.println (charNumber);
 
 		int toMove = 4 * charNumber;
 		int diff = toMove - lastChar;
 
-		Serial.print ("abs diff:");
-		Serial.println (abs (diff));
+		//Serial.print ("abs diff:");
+		//Serial.println (abs (diff));
 
 		if (abs (diff) != 0)
 		{
-			Serial.print ("abs diff > 0");
+			//Serial.print ("abs diff > 0");
 			dial.move (toMove - lastChar + 8);
 			delay (30);
 			dial.move (-8);
 		}
 
-		Serial.println ((toMove - lastChar) / 4);
+		//Serial.println ((toMove - lastChar) / 4);
 		lastChar = toMove;
 		delay (20);
 		hammerHit ();
@@ -203,13 +311,10 @@ void printChar (char charToPrint) {
 		delay (1);
 		tape.move (-12);
 		tape.disable ();
-	} else {
-		// This would execute when the character to print is not in the charTable
-		printChar('?');
 	}
-	Serial.print ("currentHeadPos: ");
-	Serial.println (currentHeadPos);
-	Serial.print ("\n\n\n");
+	//Serial.print ("currentHeadPos: ");
+	//Serial.println (currentHeadPos);
+	//Serial.print ("\n\n\n");
 	head.disable ();
 }
 
@@ -217,8 +322,7 @@ void printChar (char charToPrint) {
    Method for printing a whole string. It uses the printChar method from above for each char in the string
  */
 	void
-printString (String stringToPrint, bool ln)
-{
+printString (String stringToPrint, bool ln){
 	for (size_t i = 0; i < stringToPrint.length (); i++)
 	{
 		if (currentHeadPos >= maxHeadPos)
@@ -237,26 +341,22 @@ printString (String stringToPrint, bool ln)
    A xy move relative to the current location (no absolut coords)
  */
 	void
-xyMove (int x, int y)
-{
-	head.enable ();
-	feed.enable ();
+xyMove (int x, int y){
+	//head.enable ();
+	//feed.enable ();
 	head.move (x);
 	feed.move (y);
-	head.disable ();
-	feed.disable ();
+	//head.disable ();
+	//feed.disable ();
 }
 
 /*
    Reset the dail and return to the last head pos if needed
  */
 
-
-	void
-setup ()
-{
+void setup (){
 	Serial.begin (9600);
-	Serial.print ("Ready...");
+	//Serial.print ("Ready...");
 
 	feed.begin (RPM, MICROSTEPS);
 	tape.begin (RPM, MICROSTEPS);
@@ -291,10 +391,9 @@ setup ()
 	void
 printPixelRelativ (int x, int y)
 {
-	head.move (6 * x);
-	currentHeadPos += (6 * x);
-	feed.enable ();
-	feed.move (-5 * y);
+	head.move (x);
+	currentHeadPos += (x);
+	feed.move (y);
 
 	int toMove = 4 * charTable.lastIndexOf ('.');
 	int diff = toMove - lastChar;
@@ -321,24 +420,19 @@ printPixelRelativ (int x, int y)
 
 }
 
-	void
-print2D (int sizeX, int sizeY){
+void print2D (int sizeX, int sizeY){
 	for (int i = 0; i < sizeY; i++){
 		int blankSpace = 0;
 		for (int j = 0; j < sizeX; j++)
 		{
-			if (1)
-			{
+			if (1){
 				head.move (blankSpace);
 				currentHeadPos += blankSpace;
 				blankSpace = 0;
 				delay (1);
 				printPixelRelativ (0, 0);
 				blankSpace += 6;
-
-			}
-			else
-			{
+			}else{
 				blankSpace += 6;
 			}
 		}
@@ -353,11 +447,6 @@ print2D (int sizeX, int sizeY){
 	feed.move ((-5 * sizeY) + 20);
 }
 
-
-
-	void
-loop ()
-{
-
-	recvWithEndMarker ();
+void loop (){
+	recvSerialData ();
 }
